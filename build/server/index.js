1e6ac02f824147dccd548e77ededaa19f47536ce
user1 <noreply>
2025-08-25 03:10:35 +0900

    first commit

---
+++ build/server/index.js
@@ -0,0 +1,9 @@
+import { B, h } from "./assets/index-CxdFjusJ.js";
+import "node:events";
+import "node:stream";
+import "node:buffer";
+import "cloudflare:workers";
+export {
+  B as BooksService,
+  h as default
+};
