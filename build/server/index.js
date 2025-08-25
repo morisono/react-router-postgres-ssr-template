833c669e5d03186afe3e799eb7bfd8f0482d6a01
user1 <noreply>
2025-08-25 17:55:37 +0900

    clean build cache

--- build/server/index.js
+++
@@ -1,9 +0,0 @@
-import { B, h } from "./assets/index-DN7SCn9i.js";
-import "node:events";
-import "node:stream";
-import "node:buffer";
-import "cloudflare:workers";
-export {
-  B as BooksService,
-  h as default
-};
