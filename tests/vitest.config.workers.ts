1d61981cdf9e3c0629d55b0094b95a0c0adf66ad
user1 <noreply>
2025-08-25 18:46:11 +0900

    fix paths tests

--- tests/vitest.config.workers.ts
+++ tests/vitest.config.workers.ts
@@ -1,6 +1,4 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import defineWorkersConfig from '@cloudflare/vitest-pool-workers/config';
+import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config';
 
 export default defineWorkersConfig({
   test: {
@@ -9,7 +7,7 @@ export default defineWorkersConfig({
     poolOptions: {
       workers: {
         wrangler: {
-          configPath: path.resolve(fileURLToPath(import.meta.url), '../../wrangler.jsonc'),
+          configPath: '../wrangler.jsonc',
           environment: 'test',
         },
       },
